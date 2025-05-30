91d48df610b245029adb1154b4b4bf372baac17a
Jeff Hong <jeffhong@amazon.com>
2025-05-30 18:34:47 -0500

    store chat server configuration in config file

--- PUZZLEBOX/ChatServerConfiguration.cs
+++ PUZZLEBOX/ChatServerConfiguration.cs
@@ -1,11 +1,37 @@
 ﻿namespace PUZZLEBOX;
 
+/// <summary>
+/// Configuration for the chat server component.
+/// </summary>
 public class ChatServerConfiguration
 {
+    /// <summary>
+    /// The address that clients will use to connect to the chat server.
+    /// </summary>
     public readonly string Address;
+    
+    /// <summary>
+    /// The port used for client connections.
+    /// </summary>
     public readonly short ClientPort;
+    
+    /// <summary>
+    /// The port used for game server connections.
+    /// </summary>
     public readonly short ServerPort;
+    
+    /// <summary>
+    /// The port used for manager connections.
+    /// </summary>
     public readonly short ManagerPort;
+
+    /// <summary>
+    /// Creates a new chat server configuration with the specified parameters.
+    /// </summary>
+    /// <param name="address">The address that clients will use to connect to the chat server.</param>
+    /// <param name="clientPort">The port used for client connections.</param>
+    /// <param name="serverPort">The port used for game server connections.</param>
+    /// <param name="managerPort">The port used for manager connections.</param>
     public ChatServerConfiguration(string address, short clientPort, short serverPort, short managerPort)
     {
         Address = address;
